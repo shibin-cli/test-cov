404c03b37d0a64f2fc918af925731c88c256ec10
shibin <shibin.you@qq.com>
2021-11-09 23:12:57 +0800

    test: 💍 test ci

--- tests/sum.test.js
+++ tests/sum.test.js
@@ -1,5 +1,11 @@
-const {sum} = require('../src/utils/index')
+const sum = require('../src/index')
 
 test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
-});
+    expect(sum(1, 2)).toBe(3);
+})
+test('adds 2 + 3 to equal 5', () => {
+    expect(sum(2, 3)).toBe(5);
+})
+test('adds 11 + 29 to equal 40', () => {
+    expect(sum(11, 29)).toBe(40);
+})
